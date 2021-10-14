c6d368c14cb9d802b111bf9fc9c99951e7624785
andrew-saydjari <saydjari@g.harvard.edu>
2021-10-13 20:54:52 -0400

    defining package names

--- test/tst_utils.jl
+++ test/tst_utils.jl
@@ -1,8 +1,10 @@
 module utils_tests
     using Test
+    using disCovErr
+    
     @testset "CovEst" begin
         X, Y = cov_construct(ones(256,256),[128],[128])
         @test (X == zeros(1,33^2,33^2)).&(Y==ones(1,33^2))
-        
+
     end
 end
