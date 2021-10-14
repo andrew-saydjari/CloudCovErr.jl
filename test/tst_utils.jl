b8cda0cb978007323fa050d767d8fd66b8cd5867
andrew-saydjari <saydjari@g.harvard.edu>
2021-10-13 20:59:24 -0400

    add imageFiltering dep

--- test/tst_utils.jl
+++ test/tst_utils.jl
@@ -1,7 +1,8 @@
 module utils_tests
     using Test
     using disCovErr
-    
+    using ImageFiltering
+
     @testset "CovEst" begin
         X, Y = cov_construct(ones(256,256),[128],[128])
         @test (X == zeros(1,33^2,33^2)).&(Y==ones(1,33^2))
