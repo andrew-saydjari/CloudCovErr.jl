f20229d9899bd1c2d6bdea4d334272e253f2d70d
andrew-saydjari <saydjari@g.harvard.edu>
2021-10-17 19:56:44 -0400

    export the functions

--- src/disCovErr.jl
+++ src/disCovErr.jl
@@ -8,7 +8,18 @@ module disCovErr
     export gen_mask_staticPSF!
     export condCovEst_wdiag
     export stamp_cutter
+    export read_decam
+    export read_crowdsource
+    export proc_ccd
+    export gen_mask_staticPSF!
+    export prelim_infill!
+    export add_sky_noise!
+    export load_psfmodel_cs
+    export stamp_cutter
+    export gen_pix_mask
+    export condCovEst_wdiag
 
     include("utils.jl")
     include("decam.jl")
+    include("decaps2.jl")
 end
