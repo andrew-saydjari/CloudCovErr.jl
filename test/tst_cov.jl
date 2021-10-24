c53d52d04c63499c7688b798f38a996ebfbd387f
andrew-saydjari <saydjari@g.harvard.edu>
2021-10-24 04:39:52 -0400

    added padding test of cov_construct (up to single pix)

--- test/tst_cov.jl
+++ test/tst_cov.jl
@@ -6,5 +6,14 @@ module tst_cov
         X, Y = cov_construct(ones(256,256),[128],[128])
         @test (X == zeros(1,33^2,33^2)).&(Y==ones(1,33^2))
 
+        Np = 5
+        Δx = 2
+        Δy = 2
+        img = collect(reshape(1:400,20,20))
+        (sx, sy) = size(img)
+        @test (cov_construct(img, [1], [10])[1][1,1,1] - 11982.952403846151) < 1e-7
+        @test (cov_construct(img, [20], [10])[1][1,1,1] - 11982.952403846151) < 1e-7
+        @test (cov_construct(img, [25], [10])[1][1,1,1] - 11981.526682692307) < 1e-7
+        @test (cov_construct(img, [-5], [10])[1][1,1,1] - 11983.214182692302) < 1e-7
     end
 end
