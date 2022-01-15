ce12270bdf7302377bf678d6741225bd86dea3f1
andrew-saydjari <saydjari@g.harvard.edu>
2022-01-15 01:12:13 -0500

    it was an errant \

--- src/decam.jl
+++ src/decam.jl
@@ -29,7 +29,7 @@ function for obtaining the position dependent psf to the python namespace.
 """
 function __init__()
     if !haskey(Conda._installed_packages_dict(),"crowdsourcephoto")
-        Conda.add("crowdsourcephoto",channel="conda-forge")\
+        Conda.add("crowdsourcephoto",channel="conda-forge")
         Conda.add("libgcc",channel="conda-forge")
     end
     decam_dir = dirname(@__FILE__)*"/decam_dir"
