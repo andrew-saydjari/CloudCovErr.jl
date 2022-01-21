5c4f9be694706c9ee5a4a6398a3a54de5991d4a0
andrew-saydjari <saydjari@g.harvard.edu>
2022-01-21 02:12:42 -0500

    set decam_dir manually

--- test/tst_decam.jl
+++ test/tst_decam.jl
@@ -14,9 +14,11 @@ module tst_cov
         @test d_im[1,1] == 1
         @test abs(ref_im[1]-416.4855651855) < 1e-7
 
+        decam_dir = dirname(pwd())*"/src/decam_dir"
+
         py"""
         import os
-        os.environ['DECAM_DIR'] = $(CloudCovErr.decam.decam_dir)
+        os.environ['DECAM_DIR'] = $decam_dir
         """
         ref_im, d_im = CloudCovErr.read_decam(test_dir*"/data/decaps/c4d_","170119_085651","r","v1","S7";corrects7=true)
         @test d_im[1,1] == 1
