7b9f6f7a79eafaf446a2bba881bf0cc87810be27
Andrew Saydjari <saydjari@g.harvard.edu>
2023-02-08 02:39:24 -0500

    Update decam.jl

--- src/decam.jl
+++ src/decam.jl
@@ -32,9 +32,9 @@ function for obtaining the position dependent psf to the python namespace.
 function __init__()
     if !haskey(Conda._installed_packages_dict(),"crowdsourcephoto")
         PyCall.Conda.add(["python=3.9","crowdsourcephoto"])
+        Pkg.build("PyCall")
     end
     decam_dir = dirname(@__FILE__)*"/decam_dir"
-    println(PyCall.conda)
     py"""
     import sys
     import crowdsource.psf as psfmod
