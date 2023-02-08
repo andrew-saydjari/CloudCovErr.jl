0cd2c544515a64cb33e1bf5cebb0997777831a1e
Andrew Saydjari <saydjari@g.harvard.edu>
2023-02-08 01:01:06 -0500

    force python 3.9 to help conda solver
    I cannot wait for mamba

--- src/decam.jl
+++ src/decam.jl
@@ -31,7 +31,7 @@ function for obtaining the position dependent psf to the python namespace.
 """
 function __init__()
     if !haskey(Conda._installed_packages_dict(),"crowdsourcephoto")
-        Conda.add("crowdsourcephoto")
+        Conda.add(["python=3.9","crowdsourcephoto"])
     end
     decam_dir = dirname(@__FILE__)*"/decam_dir"
     py"""
