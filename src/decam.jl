e4c084ff17a4c0ca6767a10d47d209a6b543b082
Andrew Saydjari <saydjari@g.harvard.edu>
2023-09-03 21:41:05 -0400

    Update decam.jl

--- src/decam.jl
+++ src/decam.jl
@@ -31,7 +31,7 @@ function for obtaining the position dependent psf to the python namespace.
 """
 function __init__()
     if !haskey(Conda._installed_packages_dict(),"crowdsourcephoto")
-        PyCall.Conda.add(["python=3.9","crowdsourcephoto","scipy=1.6.1","astropy=5.2.2"])
+        PyCall.Conda.add(["python=3.9","crowdsourcephoto=0.5.6","scipy=1.6.1","astropy=5.2.2"])
     end
     decam_dir = dirname(@__FILE__)*"/decam_dir"
     py"""
